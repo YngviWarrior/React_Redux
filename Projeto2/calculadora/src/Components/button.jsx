e40f968010ae1299e54c5f224180d2a7199403e0
YngvirWarrior <igoras_ft@gmail.com>
2019-07-10 11:47:15 -0300

    Fim modulo3 calculadora

---
+++ Projeto2/calculadora/src/Components/button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './button.css'
+
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    return(
+        <button 
+            className={classes}
+            onClick={e => props.click && props.click(props.label)}>
+            {props.label}
+        </button>
+    )
+}
