b8728ad3ef878d2e4c895aca0b47096593eb5cfc
YngvirWarrior <igoras_ft@gmail.com>
2019-07-19 16:26:11 -0300

    Grid

--- todo-app/frontend/src/template/grid.jsx
+++ todo-app/frontend/src/template/grid.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Grid extends Component {
     toCssClasses(numbers){
         //Celular Tablet e telas maiores
-        const cols = numbers ? numbers.split(' ') : []
-        let classes = ''
+        const cols = numbers ? numbers.split(" ") : []
+        let classes = ""
 
         if(cols[0]) classes+= `col-xs-${cols[0]}`
         if(cols[1]) classes+= `col-sm-${cols[1]}`
